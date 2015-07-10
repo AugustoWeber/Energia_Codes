096719aedff497d2f440e29945c31828ca493f00
Augusto <augusto.weber@ecomp.ufsm.br>
2015-07-10 16:09:40 -0300

    UP Teste Serial

--- Teste_Serial_v2/Teste_Serial_v2.ino
+++ Teste_Serial_v2/Teste_Serial_v2.ino
@@ -6,35 +6,30 @@
  * Flow control: none
  */
 
-
-//char value[256];
-//char Buffer[256];
-//String buffer;
-//String bufferSerial;
 char inByte;
 char inByteS;
 int cont;
-//int i;
+
 #define RED    RED_LED
 #define GREEN  GREEN_LED
 #define BLUE   BLUE_LED
 
 void setup()
 {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
   Serial2.begin(9600);
   Serial2.setTimeout(5000);
   pinMode(RED,OUTPUT);
   pinMode(GREEN,OUTPUT);
   pinMode(BLUE,OUTPUT);
-  digitalWrite(RED,128);
+  digitalWrite(RED,0);
   digitalWrite(GREEN,0);
   digitalWrite(BLUE,0);
   cont = 0;
   inByte = 0;
   inByteS = 0;
-  //CleanBuffer(Buffer);
+  
 }
 
 void loop()
@@ -43,13 +38,13 @@ void loop()
     inByteS = Serial.read();
     Serial2.write(inByteS);
   }
-  //delay(50);
+  
   if(Serial2.available()){
     cont++;
     inByte = Serial2.read();
     Serial.print(inByte);
   }
-
+/*
   switch(cont){
   case 0: 
     digitalWrite(RED,128);
@@ -75,6 +70,5 @@ void loop()
     digitalWrite(BLUE,0);
     cont = 0;
     break;
-  } 
-  delay(50);
+  } */
 }
